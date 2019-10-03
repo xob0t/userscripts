9b2ba93912b2ef43204bc3c1acc32786486820c4
xob0t <32616886+xob0t@users.noreply.github.com>
2019-10-03 10:19:26 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         test
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  \
+// @author       
+// @match        https://www.reddit.com/*
+// @grant        none
+// ==/UserScript==
+
+
+
+let padgeURL = window.location.href;
+console.log("url", padgeURL);
