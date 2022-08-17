182c6dbb300cd6484609f9aead965ff79d42a4fe
xob0t <32616886+xob0t@users.noreply.github.com>
2022-08-17 12:47:48 +0300

    Create vkads.js

---
+++ vkads.js
@@ -0,0 +1,35 @@
+
+// ==UserScript==
+// @name         Vk ads hide
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  x
+// @author       me
+// @match        https://vk.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=vk.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function getElementByXpath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+const waitInterval = setInterval(async () => {
+
+try {
+    let wall_marked_as_ads = getElementByXpath("//div[not(@hidden)][@data-post-id][.//div[@class='wall_marked_as_ads']]")
+    wall_marked_as_ads.style.display = "none";
+    wall_marked_as_ads.setAttribute('hidden', 'hidden');
+    console.log('реклама спрятана');
+    } catch(e) {};
+try {
+    let ad_post = getElementByXpath("//div[not(@hidden)][@data-ad-block-uid]");
+    ad_post.style.display = "none";
+    ad_post.setAttribute('hidden', 'hidden');
+    console.log('реклама спрятана');
+    } catch(e) {};
+    }, 100);
+
+})();
