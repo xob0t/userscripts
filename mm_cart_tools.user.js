d03bc6daf18494832f97c4c55efc2651747d574d
xob0t <32616886+xob0t@users.noreply.github.com>
2023-12-21 11:13:52 +0300

    optimization, field removed

--- mm_cart_tools.user.js
+++ mm_cart_tools.user.js
@@ -6,232 +6,259 @@
 // @author       xob0t
 // @match        https://megamarket.ru/multicart/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=megamarket.ru
-// @grant        none
+// @grant        GM_setClipboard
 // @run-at       document-end
 // ==/UserScript==
 
 (function() {
 
-    'use strict';
+	'use strict';
 
-    // Create a container div for button, input field, and submit button
-    var container = document.createElement('div');
-    container.style.position = 'fixed';
-    container.style.top = '20px';
-    container.style.right = '20px';
-    container.style.zIndex = '9999';
-    container.style.display = 'flex';
-    container.style.justifyContent = 'space-between';
-    container.style.alignItems = 'center';
+	var container = null;
+	var copyButton = null;
+	var readButton = null;
 
-    // Create an input field for user input
-    var inputField = document.createElement('input');
-    inputField.type = 'text';
-    inputField.placeholder = 'Enter cart data';
-    inputField.style.height = '40px'; // Increase height to match buttons
-    inputField.style.flex = '1'; // Take remaining space
-    inputField.style.marginRight = '10px';
+	function addButtonContainer() {
+		if (window.location.href === 'https://megamarket.ru/multicart/') {
+			if (!container) {
+				// Create a container div for button, input field, and submit button
+				container = document.createElement('div');
+				container.id = 'cartToolsContainer'; // Set an ID for easy identification
 
-    // Create a submit button for user input
-    var submitButton = document.createElement('button');
-    submitButton.textContent = 'Submit';
-    submitButton.style.padding = '10px';
-    submitButton.style.backgroundColor = 'blue';
-    submitButton.style.color = 'white';
-    submitButton.style.border = 'none';
-    submitButton.style.borderRadius = '5px';
+				container.style.position = 'fixed';
+				container.style.top = '20px';
+				container.style.right = '20px';
+				container.style.zIndex = '9999';
+				container.style.display = 'flex';
+				container.style.justifyContent = 'space-between';
+				container.style.alignItems = 'center';
 
-    // Create a button element for copying stringified array of currentItems
-    var copyButton = document.createElement('button');
-    copyButton.textContent = 'Copy Cart';
-    copyButton.style.padding = '10px';
-    copyButton.style.backgroundColor = 'green';
-    copyButton.style.color = 'white';
-    copyButton.style.border = 'none';
-    copyButton.style.borderRadius = '5px';
-    copyButton.style.marginRight = '10px';
+				readButton = document.createElement('button');
+				readButton.textContent = 'Read Clipboard';
+				readButton.style.padding = '10px';
+				readButton.style.backgroundColor = 'orange';
+				readButton.style.color = 'white';
+				readButton.style.border = 'none';
+				readButton.style.borderRadius = '5px';
+				readButton.style.marginRight = '10px';
 
-    // Append copyButton and submitButton to the container
-    container.appendChild(copyButton);
-    container.appendChild(submitButton);
-    container.appendChild(inputField);
+				// Create a button element for copying stringified array of currentItems
+				copyButton = document.createElement('button');
+				copyButton.textContent = 'Copy Cart';
+				copyButton.style.padding = '10px';
+				copyButton.style.backgroundColor = 'green';
+				copyButton.style.color = 'white';
+				copyButton.style.border = 'none';
+				copyButton.style.borderRadius = '5px';
+				copyButton.style.marginRight = '10px';
 
-    // Append container to the body
-    document.body.appendChild(container);
+				// Append copyButton and submitButton to the container
+				container.appendChild(copyButton);
+				container.appendChild(readButton);
 
-    // Function to handle button click to copy stringified array of currentItems
-    function handleCopy() {
-        var dataLayerCart = dataLayer.find(item => item.cart);
-        if (dataLayerCart) {
-            var cartId = dataLayerCart.cart.lineItems[0].cartId;
-            getCartData(cartId)
-                .then(cart => {
-                var currentItems = extractItemsAndCartInfo(cart);
-                var currentItemsStringified = JSON.stringify(currentItems);
-                copyToClipboard(currentItemsStringified);
-                copyButton.textContent = 'Copied!'; // Change button text when copied
-                copyButton.style.backgroundColor = 'orange';
-                setTimeout(function() {
-                    copyButton.textContent = 'Copy Cart'; // Revert button text after a brief delay
-                    copyButton.style.backgroundColor = 'green';
-                }, 3000); // Change button text back after 3 seconds
-            })
-                .catch(error => {
-                console.error("Error occurred while fetching cart data:", error);
-            });
-        } else {
-            console.log('Cart JSON not found in the dataLayer array. Retrying...');
-            setTimeout(checkForCart, 1000); // Check again after 1 second
-        }
-    }
+				// Append container to the body
+				document.body.appendChild(container);
 
-    // Function to handle user input and addToCart if needed
-    function handleSubmit() {
-        var enteredText = inputField.value.trim();
-        if (enteredText !== '') {
-            var newCartData;
-            try {
-                newCartData = JSON.parse(enteredText);
-            } catch (error) {
-                console.error('Invalid input. Please enter a valid JSON.');
-                return;
-            }
+				// Add click event listener to the copyButton
+				copyButton.addEventListener('click', handleCopy);
+				readButton.addEventListener('click', readClipboard);
+			}
+		} else {
+			removeButtonContainer();
+		}
+	}
 
-            var dataLayerCart = dataLayer.find(item => item.cart);
-            var cartId = null; // Initialize cartId as null
+	function removeButtonContainer() {
+		var existingContainer = document.getElementById('cartToolsContainer');
+		if (existingContainer) {
+			existingContainer.parentNode.removeChild(existingContainer);
+			container = null;
+		}
+	}
 
-            var currentItems = []; // Initialize currentItems as an empty array
+	// Initial check and addition of buttons
+	addButtonContainer();
 
-            if (dataLayerCart && dataLayerCart.cart && dataLayerCart.cart.lineItems && dataLayerCart.cart.lineItems.length > 0) {
-                cartId = dataLayerCart.cart.lineItems[0].cartId;
-            }
+	// Monitor changes in the page using MutationObserver
+	var observer = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+			addButtonContainer(); // Recheck and manage buttons on any changes in the page content
+		});
+	});
 
-            if (cartId) {
-                getCartData(cartId)
-                    .then(cart => {
-                    addToCart(newCartData.extractedItems, cartId, newCartData.extractedCartInfo.type, newCartData.extractedCartInfo.locationId);
-                    location.reload();
-                })
-                    .catch(error => {
-                    console.error("Error occurred while fetching cart data:", error);
-                });
-            } else {
-                console.log('Cart ID not found or invalid. Proceeding without cartId.');
-                addToCart(newCartData.extractedItems, cartId, newCartData.extractedCartInfo.type, newCartData.extractedCartInfo.locationId, newCartData.extractedCartInfo.clientAddress);
-                location.reload();
-                // Perform an action here if cartId is not available
-                // For example, you might want to handle adding items to the cart without a cartId
-            }
-        } else {
-            console.log('Please enter valid data.');
-        }
-    }
+	// Configuration of the observer: observe changes in the body element and its subtree
+	var observerConfig = {
+		childList: true,
+		subtree: true
+	};
+	observer.observe(document.body, observerConfig);
 
-    // Add click event listener to the copyButton
-    copyButton.addEventListener('click', handleCopy);
+	function addToCart(newCartData) {
+		var dataLayerCart = dataLayer.find(item => item.cart);
+		var cartId = null; // Initialize cartId as null
 
-    // Add click event listener to the submitButton
-    submitButton.addEventListener('click', handleSubmit);
+		var currentItems = []; // Initialize currentItems as an empty array
 
+		if (dataLayerCart && dataLayerCart.cart && dataLayerCart.cart.lineItems && dataLayerCart.cart.lineItems.length > 0) {
+			cartId = dataLayerCart.cart.lineItems[0].cartId;
+		}
 
-    function extractItemsAndCartInfo(data) {
-        const extractedItems = data.itemGroups.map(item => {
-            return {
-                offer: {
-                    id: null,
-                    merchantId: item.merchant.id ? parseInt(item.merchant.id) : null
-                },
-                goods: {
-                    goodsId: item.goods.goodsId
-                },
-                quantity: item.quantity,
-                isBpg20: false,
-                discounts: []
-            };
-        });
+		addToCartRequest(newCartData.extractedItems, cartId, newCartData.extractedCartInfo.type, newCartData.extractedCartInfo.locationId, newCartData.extractedCartInfo.clientAddress);
+		location.reload();
+	}
 
-        const extractedCartInfo = {
-            type: data.type,
-            locationId: data.locationId,
-            clientAddress: data.clientAddress,
-        };
+	function readClipboard() {
+		navigator.clipboard.readText()
+			.then(text => {
+				const trimmedText = text.trim();
+				if (trimmedText !== '') {
+					try {
+						const jsonObject = JSON.parse(trimmedText);
+						console.log(jsonObject)
+						addToCart(jsonObject)
+					} catch (error) {
+						alert('Clipboard content is not valid JSON.');
+					}
+				} else {
+					alert('Clipboard is empty.');
+				}
+			})
+			.catch(err => {
+				console.error('Failed to read clipboard contents: ', err);
+			});
+	}
+	// Function to handle button click to copy stringified array of currentItems
+	function handleCopy() {
+		var dataLayerCart = dataLayer.find(item => item.cart);
+		if (dataLayerCart) {
+			var cartId = dataLayerCart.cart.lineItems[0].cartId;
+			getCartData(cartId)
+				.then(cart => {
+					var currentItems = extractItemsAndCartInfo(cart);
+					console.log(currentItems);
+					var currentItemsStringified = JSON.stringify(currentItems);
 
-        return {
-            extractedItems,
-            extractedCartInfo
-        };
-    }
-    function addToCart(items, cartId,cartType, locationId, clientAddress) {
-    fetch("https://megamarket.ru/api/mobile/v2/cartService/offers/add", {
-        "body": JSON.stringify({
-            "identification": {
-                "id": cartId
-            },
-            "items": items,
-            "cartType": cartType,
-            "clientAddress": clientAddress,
-            "locationId": locationId
-        }),
-        "method": "POST",
-        "mode": "cors",
-        "credentials": "include"
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("Cart Data:", data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
-
-        // Function to copy text to clipboard
-    function copyToClipboard(text) {
-        var textarea = document.createElement("textarea");
-        textarea.value = text;
-        document.body.appendChild(textarea);
-        textarea.select();
-        document.execCommand("copy");
-        document.body.removeChild(textarea);
-    }
+					GM.setClipboard(currentItemsStringified);
+					//copyToClipboard(currentItemsStringified);
+					copyButton.textContent = 'Copied!'; // Change button text when copied
+					copyButton.style.backgroundColor = 'orange';
+					setTimeout(function() {
+						copyButton.textContent = 'Copy Cart'; // Revert button text after a brief delay
+						copyButton.style.backgroundColor = 'green';
+					}, 3000); // Change button text back after 3 seconds
+				})
+				.catch(error => {
+					alert("Error occurred while fetching cart data:", error);
+				});
+		} else {
+			console.log('Cart JSON not found in the dataLayer array.');
+		}
+	}
 
 
-    function getCartData(cartId) {
-        return fetch("https://megamarket.ru/api/mobile/v2/cartService/cart/get", {
-            method: "POST",
-            mode: "cors",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "identification": {
-                    "id": cartId
-                },
-                "isCartStateValidationRequired": true,
-                "isSelectedItemGroupsOnly": false,
-                "loyaltyCalculationRequired": true,
-                "isSkipPersonalDiscounts": true
-            })
-        })
-            .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok.');
-            }
-            return response.json();
-        })
-            .catch(error => {
-            console.error('Error:', error);
-            throw error;
-        });
-    }
+	function extractItemsAndCartInfo(data) {
+		const extractedItems = data.itemGroups.map(item => {
+			return {
+				offer: {
+					id: null,
+					merchantId: item.merchant.id ? parseInt(item.merchant.id) : null
+				},
+				goods: {
+					goodsId: item.goods.goodsId
+				},
+				quantity: item.quantity,
+				isBpg20: false,
+				discounts: []
+			};
+		});
 
-    function allItemsInCurrentItems(arr1, arr2) {
-        const arr1Str = arr1.map(item => JSON.stringify(item));
-        const arr2Str = arr2.map(item => JSON.stringify(item));
+		const extractedCartInfo = {
+			type: data.type,
+			locationId: data.locationId,
+			clientAddress: data.clientAddress,
+		};
 
-        return arr2Str.every(item => arr1Str.includes(item));
-    }
+		return {
+			extractedItems,
+			extractedCartInfo
+		};
+	}
+
+	function addToCartRequest(items, cartId, cartType, locationId, clientAddress) {
+		fetch("https://megamarket.ru/api/mobile/v2/cartService/offers/add", {
+				"body": JSON.stringify({
+					"identification": {
+						"id": cartId
+					},
+					"items": items,
+					"cartType": cartType,
+					"clientAddress": clientAddress,
+					"locationId": locationId
+				}),
+				"method": "POST",
+				"mode": "cors",
+				"credentials": "include"
+			})
+			.then(response => response.json())
+			.then(data => {
+				console.log("Cart Data:", data);
+			})
+			.catch(error => {
+				console.error('Error:', error);
+			});
+	}
+
+	function getAllCarts() {
+		return fetch("https://megamarket.ru/api/mobile/v2/cartService/cart/search", {
+				method: "POST",
+				mode: "cors",
+				credentials: "include",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: ""
+			})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok.');
+				}
+				return response.json();
+			})
+			.catch(error => {
+				console.error('Error:', error);
+				throw error;
+			});
+	}
+
+	function getCartData(cartId) {
+		return fetch("https://megamarket.ru/api/mobile/v2/cartService/cart/get", {
+				method: "POST",
+				mode: "cors",
+				credentials: "include",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					"identification": {
+						"id": cartId
+					},
+					"isCartStateValidationRequired": true,
+					"isSelectedItemGroupsOnly": false,
+					"loyaltyCalculationRequired": true,
+					"isSkipPersonalDiscounts": true
+				})
+			})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok.');
+				}
+				return response.json();
+			})
+			.catch(error => {
+				console.error('Error:', error);
+				throw error;
+			});
+	}
 
 
 })();
