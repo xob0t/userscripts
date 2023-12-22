b30895707497e9f6efd2709d7f99e4e1b0049781
xob0t <32616886+xob0t@users.noreply.github.com>
2023-12-22 20:49:54 +0300

    major rework

--- mm_cart_tools.user.js
+++ mm_cart_tools.user.js
@@ -1,26 +1,25 @@
 // ==UserScript==
 // @name         MM cart tools
-// @namespace    http://tampermonkey.net/
-// @version      2023-12-21
-// @description  copy and paste cart items
+// @namespace    https://github.com/xob0t/MM-tools
+// @version      2023-12-22
+// @description  Копирование, вставка и удаление корзин
 // @author       xob0t
 // @match        https://megamarket.ru/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=megamarket.ru
 // @grant        GM_setClipboard
-// @run-at       document-end
+// @run-at       document-start
 // ==/UserScript==
 
-(function() {
+(function () {
 	'use strict';
 
 	const createDeleteButton = () => {
 		const button = document.createElement('button');
 		button.setAttribute('type', 'button');
-		button.classList.add('btn', 'btn-block', 'btn-checkout', 'btn-delete'); // Adding classes
-		button.textContent = 'Удалить корзину'; // Text for the button
+		button.classList.add('btn', 'btn-block', 'btn-checkout', 'btn-delete');
+		button.textContent = 'Удалить корзину';
 		button.style.backgroundColor = "brown";
 
-		// Adding event listener to the created button
 		button.addEventListener('click', () => {
 			handleDeleteCartButtonClick(button);
 		});
@@ -32,7 +31,6 @@
 		const parentCart = button.closest('.multicart-item.cart.multicart__item');
 		let removeButtons = parentCart.querySelectorAll('.good__remove');
 
-		// Add CSS transition to the button for smoother visual changes
 		button.style.transition = 'background-color 0.5s ease-in-out, color 0.5s ease-in-out';
 
 		while (removeButtons.length > 0) {
@@ -44,20 +42,17 @@
 						button.style.transition = 'background-color 0.5s ease-in-out';
 						removeButton.click(); // Trigger click on each element with class "good__remove"
 						resolve();
-					}, 100); // Adjust the delay time as needed (in milliseconds)
+					}, 100);
 				});
 
 				// Restore initial button appearance after deletion
 			}
 			removeButtons = parentCart.querySelectorAll('.good__remove'); // Update the NodeList
 		}
-
-		console.log('Delete Cart button clicked until no elements left');
-		// Additional logic to perform after deleting items
 	};
-	// Function to add the "Delete Cart" button to the specified element
-	const addDeleteButtonToElement = element => {
-		const summary = element.querySelector('.cart-summary-redesign__inner');
+
+	const addDeleteButtonToCart = cartElement => {
+		const summary = cartElement.querySelector('.cart-summary-redesign__inner');
 		if (summary) {
 			const checkoutButton = summary.querySelector('button.btn.btn-block.btn-checkout');
 			if (checkoutButton) {
@@ -69,58 +64,129 @@
 
 
 
-	const addNewFieldToEmptyCart = () => {
-		const cartEmptyDescription = document.querySelector('.cart-empty__description');
-		const cartInput = document.querySelector('.cart-input');
-		if (cartEmptyDescription && !cartInput) {
-			const textField = document.createElement('input');
-			textField.classList.add('cart-input');
-			textField.type = 'text';
-			textField.placeholder = 'Вставить';
-			textField.style.cssText = 'padding: 8px; margin-right: 10px; flex: 1;';
-			textField.addEventListener('keydown', event => {
-				if (event.key === 'Enter') {
-					handleTextFieldSubmit(textField);
-				}
-			});
+	const createInputFieldWithId = (id) => {
+		const divElement = document.createElement('div');
+		divElement.className = 'form-group';
+		divElement.id = id;
+		divElement.style.width = "50%"
+		divElement.style.marginTop = "30px"
+		divElement.style.marginBottom = "30px"
 
-			// Inserting the new field element after the multicart__title element
-			cartEmptyDescription.parentNode.insertBefore(textField, cartEmptyDescription.nextSibling);
-		}
+		const inputElement = document.createElement('input');
+		inputElement.setAttribute('type', 'text');
+		inputElement.className = 'text-input';
+		inputElement.placeholder = 'Вставить корзину';
+
+		divElement.appendChild(inputElement);
+
+		inputElement.addEventListener('keydown', event => {
+			if (event.key === 'Enter') {
+				handleTextFieldSubmit(inputElement);
+			}
+		});
+		return divElement
 	};
 
-	const addNewFieldAfterTitle = () => {
-		const multicartTitle = document.querySelector('.multicart__title');
-		if (multicartTitle) {
-			const textField = document.createElement('input');
-			textField.type = 'text';
-			textField.placeholder = 'Вставить';
-			textField.style.cssText = 'padding: 8px; margin-right: 10px; flex: 1;';
-			textField.addEventListener('keydown', event => {
-				if (event.key === 'Enter') {
-					handleTextFieldSubmit(textField);
-				}
-			});
-
-			// Inserting the new field element after the multicart__title element
-			multicartTitle.parentNode.insertBefore(textField, multicartTitle.nextSibling);
-		}
-	};
-
-	const handleTextFieldSubmit = (textField) => {
-		const trimmedText = textField.value.trim();
+	const handleTextFieldSubmit = async (inputField) => {
+		const trimmedText = inputField.value.trim();
 		if (trimmedText !== '') {
 			try {
 				const newCartData = JSON.parse(trimmedText);
-				addToCartRequest(newCartData.extractedItems, newCartData.extractedCartInfo.type, newCartData.extractedCartInfo.locationId);
+				await addToCartRequest(newCartData.items, newCartData.cartInfo.type, newCartData.cartInfo.locationId);
+				location.reload()
 			} catch (error) {
 				console.error('Error parsing JSON:', error);
-				alert('Entered text is not valid JSON.');
+				alert('Ошибка ввода корзины');
 			}
 		}
 	};
 
-	const addToCartRequest = (items, cartType, locationId) => {
+	const removeInputFieldWithId = (id) => {
+		const elementToRemove = document.getElementById(id);
+		if (elementToRemove) {
+			elementToRemove.remove();
+		} else {
+			console.log(`Element with ID ${id} not found.`);
+		}
+	};
+
+	const extractItemsAndCartInfo = (data) => {
+		const items = data.itemGroups.map(item => {
+			return {
+				offer: {
+					merchantId: item.merchant.id ? parseInt(item.merchant.id) : null
+				},
+				goods: {
+					goodsId: item.goods.goodsId
+				},
+				quantity: item.quantity
+			};
+		});
+
+		const cartInfo = {
+			type: data.type,
+			locationId: data.locationId,
+		};
+
+		return {
+			items,
+			cartInfo
+		};
+	};
+
+	const getCartData = async (cartId) => {
+		return fetch("https://megamarket.ru/api/mobile/v2/cartService/cart/get", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			credentials: "include",
+			body: JSON.stringify({
+				"identification": {
+					"id": cartId
+				},
+				"isCartStateValidationRequired": true,
+				"isSelectedItemGroupsOnly": false,
+				"loyaltyCalculationRequired": true,
+				"isSkipPersonalDiscounts": true
+			})
+		})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok.');
+				}
+				return response.json();
+			})
+			.catch(error => {
+				console.error('Error getCartData:', error);
+				throw error;
+			});
+	};
+
+	const getAllCarts = () => {
+		return fetch("https://megamarket.ru/api/mobile/v2/cartService/cart/search", {
+			method: "POST",
+			mode: "cors",
+			credentials: "include",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: null
+		})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok.');
+				}
+				return response.json();
+			})
+			.catch(error => {
+				console.error('Error:', error);
+				throw error;
+			});
+	}
+
+
+	const addToCartRequest = async (items, cartType, locationId) => {
 		const url = "https://megamarket.ru/api/mobile/v2/cartService/offers/add";
 		const requestBody = {
 			"identification": {
@@ -139,41 +205,41 @@
 			"locationId": locationId
 		};
 
-		fetch(url, {
+		try {
+			const response = await fetch(url, {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json"
 				},
 				credentials: "include",
 				body: JSON.stringify(requestBody)
-			})
-			.then(response => {
-				if (!response.ok) {
-					throw new Error("Network response was not ok");
-				}
-				return response.json();
-			})
-			.then(responseData => {
-				console.log("Cart Data:", responseData);
-				location.reload();
-			})
-			.catch(error => {
-				console.error("Error:", error.message);
 			});
+
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+
+			const responseData = await response.json();
+			console.log("Cart Data:", responseData);
+			return responseData;
+		} catch (error) {
+			console.error("Error:", error.message);
+			throw error;
+		}
 	};
 
-	const createCopyButton = (text, bgColor) => {
+	const createCopyButton = () => {
 		const button = document.createElement('button');
 		button.setAttribute('type', 'button');
-		button.classList.add('btn', 'btn-block', 'btn-checkout'); // Adding classes
+		button.classList.add('btn', 'btn-block', 'btn-checkout');
 
 		const spanElement = document.createElement('span');
-		spanElement.textContent = text; // Text for the button
+		spanElement.textContent = 'Копировать корзину';
 
-		button.appendChild(spanElement); // Appending the span element to the button
+		button.appendChild(spanElement);
 
 		// Applying styles
-		button.style.backgroundColor = bgColor;
+		button.style.backgroundColor = 'green';
 
 		// Adding event listener to the created button
 		button.addEventListener('click', async () => {
@@ -223,135 +289,101 @@
 	};
 
 	// Function to add buttons to the specified element
-	const addButtonToElement = element => {
-		const summary = element.querySelector('.cart-summary-redesign__inner');
-		if (summary) {
-			const checkoutButton = summary.querySelector('button.btn.btn-block.btn-checkout');
-			if (checkoutButton) {
-				const button = createCopyButton('Копировать корзину', 'green', () => {
-					// Add functionality for the click handler here
-					// For example, you can copy content or perform any action related to the specific item
-					console.log('Button clicked for item:', element);
-				});
-				summary.insertBefore(button, checkoutButton.nextSibling);
-			}
+	const addCopyButtonToCart = cartElement => {
+		const checkoutButton = cartElement.querySelector('button.btn.btn-block.btn-checkout');
+		if (checkoutButton) {
+			checkoutButton.parentNode.insertBefore(createCopyButton(), checkoutButton.nextSibling);
 		}
+
 	};
 
+	const addFieldToEmptyCart = (emptyCart) => {
+		const cartEmptyImage = emptyCart.querySelector(".cart-empty__image");
+		const inputField = createInputFieldWithId("emptyCart");
+
+		// Create a new div element to serve as the parent for the inputField
+		const newParent = document.createElement('div');
+
+		// Apply styles to the new parent if needed
+		newParent.style.display = 'flex'; // Set the parent's display to flex
+		newParent.style.justifyContent = 'center'; // Center content horizontally
+		newParent.style.width = '100%'; // Ensure the new parent takes the full width available
+
+		// Insert the inputField into the new parent
+		newParent.appendChild(inputField);
+
+		// Additional styles to position inputField at the horizontal center within the new parent
+		inputField.style.display = 'block'; // Ensure the inputField is a block-level element for proper centering
+		inputField.style.margin = 'auto'; // Set margin to auto to horizontally center
+		inputField.style.maxWidth = '50%'; // Set a maximum width for the inputField
+		inputField.style.width = '100%'; // Ensure the inputField takes up the available width within the new parent
+		inputField.style.textAlign = 'center'; // Center text within the inputField
+
+		// Insert the new parent with the inputField as the next sibling of cartEmptyImage
+		cartEmptyImage.parentNode.insertBefore(newParent, cartEmptyImage.nextSibling);
+
+	};
+
+	const handleNewMultiCart = (node) => {
+		const multicartList = node.querySelector('.multicart__list');
+		if (multicartList && multicartList.children) {
+			handleNewCartList(multicartList)
+		}
+	}
+
+	const handleNewCartList = (node) => {
+		for (const cartElement of node.children) {
+			// for a cart in the multicart
+			addButtonsToCart(cartElement);
+		}
+		const isCartExists = document.getElementById("cartField")
+		if (!isCartExists) {
+			node.parentNode.insertBefore(createInputFieldWithId("cartField"), node);
+		}
+
+	}
+
+	const addButtonsToCart = (cartElement) => {
+		addCopyButtonToCart(cartElement);
+		addDeleteButtonToCart(cartElement);
+	}
 
 
-	function handleNewElements(mutationsList) {
-		for (const mutation of mutationsList) {
+	const handleMutations = (mutationsList) => {
+		mutationsList.forEach(mutation => {
 			if (mutation.type === 'childList') {
 				mutation.addedNodes.forEach(node => {
-					if (node.nodeType === Node.ELEMENT_NODE) {
-						const newElements = node.querySelectorAll('.multicart-item.cart.multicart__item');
-						if (newElements.length > 0) {
-							console.log('New .multicart-item.cart.multicart__item element(s) found:', newElements);
-							newElements.forEach(element => {
-								addButtonToElement(element);
-								addDeleteButtonToElement(element);
-							});
-							const mulitiCart = document.querySelector('.multicart');
-							if (mulitiCart) {
-								addNewFieldAfterTitle();
-							}
-						}
+					if (node && node.classList && node.classList.length === 1 && node.classList.contains('multicart')) {
+						// new multicart added
+						handleNewMultiCart(node);
+					}
+					if (node && node.classList && node.classList.length === 1 && node.classList.contains('multicart__list')) {
+						// new multicart__list added
+						handleNewCartList(node);
+					}
 
-						// Run the function only if the element exists in the document
-						const cartEmpty = document.querySelector('.cart-empty');
-						if (cartEmpty) {
-							addNewFieldToEmptyCart(); // Run the function when cart-empty element is found
-						}
+					if (node.classList && node.classList.contains('cart-empty')) {
+						// new empty cart added
+						console.log('New cart-empty:', node);
+						addFieldToEmptyCart(node)
+
+					}
+				});
+
+				mutation.removedNodes.forEach(node => {
+					if (node.classList && node.classList.contains('multicart__list')) {
+						// multicart__list removed
+						removeInputFieldWithId("cartField")
 					}
 				});
 			}
-		}
-	}
-	const extractItemsAndCartInfo = (data) => {
-		const extractedItems = data.itemGroups.map(item => {
-			return {
-				offer: {
-					merchantId: item.merchant.id ? parseInt(item.merchant.id) : null
-				},
-				goods: {
-					goodsId: item.goods.goodsId
-				},
-				quantity: item.quantity
-			};
 		});
-
-		const extractedCartInfo = {
-			type: data.type,
-			locationId: data.locationId,
-		};
-
-		return {
-			extractedItems,
-			extractedCartInfo
-		};
-	};
-
-	const getCartData = (cartId) => {
-		return fetch("https://megamarket.ru/api/mobile/v2/cartService/cart/get", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json"
-				},
-				credentials: "include",
-				body: JSON.stringify({
-					"identification": {
-						"id": cartId
-					},
-					"isCartStateValidationRequired": true,
-					"isSelectedItemGroupsOnly": false,
-					"loyaltyCalculationRequired": true,
-					"isSkipPersonalDiscounts": true
-				})
-			})
-			.then(response => {
-				if (!response.ok) {
-					throw new Error('Network response was not ok.');
-				}
-				return response.json();
-			})
-			.catch(error => {
-				console.error('Error getCartData:', error);
-				throw error;
-			});
-	};
-
-	function getAllCarts() {
-		return fetch("https://megamarket.ru/api/mobile/v2/cartService/cart/search", {
-				method: "POST",
-				mode: "cors",
-				credentials: "include",
-				headers: {
-					"Content-Type": "application/json"
-				},
-				body: null
-			})
-			.then(response => {
-				if (!response.ok) {
-					throw new Error('Network response was not ok.');
-				}
-				return response.json();
-			})
-			.catch(error => {
-				console.error('Error:', error);
-				throw error;
-			});
 	}
 
-	// Creating a MutationObserver instance
-	const observer = new MutationObserver(handleNewElements);
 
-	// Configuration of the observer
-	const observerConfig = {
-		childList: true, // Listen for changes in the child elements
-		subtree: true, // Include all descendants of the target node
-	};
+	const observer = new MutationObserver(handleMutations);
 
-	// Start observing the body for changes
-	observer.observe(document.body, observerConfig);
+	const observerConfig = { childList: true, subtree: true };
+
+	observer.observe(document, observerConfig);
 })();
